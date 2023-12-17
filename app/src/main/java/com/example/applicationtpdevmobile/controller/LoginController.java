070bdd3e2fda50606e7c82650ccd4c60e61c0fc2
Taher726 <chaltouttaher@gmail.com>
2023-12-18 00:00:51 +0100

    TP6

--- app/src/main/java/com/example/applicationtpdevmobile/controller/LoginController.java
+++ app/src/main/java/com/example/applicationtpdevmobile/controller/LoginController.java
@@ -7,14 +7,19 @@ public class LoginController {
     private static final String PREF_NAME = "UserPreferences";
     private static final String KEY_USERNAME = "username";
     private static final String KEY_PASSWORD = "password";
-    private String username;
-    private String password;
+    private static User user;
 
     public static LoginController instance;
 
+    public static final LoginController getInstance(Context context){
+        if(LoginController.instance ==null){
+            LoginController.instance = new LoginController();
+        }
+        recapUser(context);
+        return LoginController.instance;
+    }
     public void createUser(String userName, String password, Context context) {
-        this.username = userName;
-        this.password = password;
+        user = new User(userName, password);
         SharedPreferences sharedPreferences = context.getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE);
         SharedPreferences.Editor editor = sharedPreferences.edit();
         editor.putString(KEY_USERNAME, userName);
@@ -23,7 +28,15 @@ public class LoginController {
     }
     public static void recapUser(Context context) {
         SharedPreferences sharedPreferences = context.getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE);
-        instance.username = sharedPreferences.getString(KEY_USERNAME, null);
-        instance.password = sharedPreferences.getString(KEY_PASSWORD, null);
+        String username = sharedPreferences.getString(KEY_USERNAME, null);
+        String password = sharedPreferences.getString(KEY_PASSWORD, null);
+        user = new User(username,password);
+    }
+    public String getUserName() {
+        return user.getNom();
+    }
+
+    public String getPassword() {
+        return user.getMdp();
     }
 }
--- app/src/main/java/com/example/applicationtpdevmobile/vue/MainActivity2.java
+++ app/src/main/java/com/example/applicationtpdevmobile/vue/MainActivity2.java
@@ -5,23 +5,61 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
 import com.example.applicationtpdevmobile.R;
+import com.example.applicationtpdevmobile.controller.LoginController;
 
 public class MainActivity2 extends AppCompatActivity {
+
     private Button btnConsultation;
+    private EditText etUserName;
+    private EditText etPassword;
+    private LoginController loginController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
-        btnConsultation = (Button) findViewById(R.id.btnConsultation);
+        init();
+
+        etUserName.setText(loginController.getUserName());
+        etPassword.setText(loginController.getPassword());
+
         btnConsultation.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity2.this, MainActivity.class);
-                startActivity(intent);
-                finish();
+                String userName, password;
+                boolean verifUserName = false, verifPassword = false;
+
+                if(!etUserName.getText().toString().isEmpty())
+                    verifUserName = true;
+                else
+                    Toast.makeText(MainActivity2.this, "Veuillez saisir votre nom d'utilisateur !", Toast.LENGTH_SHORT).show();
+
+                if(!etPassword.getText().toString().isEmpty())
+                    verifPassword = true;
+                else
+                    Toast.makeText(MainActivity2.this, "Veuillez saisir votre mot de passe !", Toast.LENGTH_LONG).show();
+
+                if(verifPassword && verifUserName) {
+                    userName = etUserName.getText().toString();
+                    password = etPassword.getText().toString();
+                    loginController.createUser(userName, password, MainActivity2.this);
+                    Intent intent = new Intent(MainActivity2.this, MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
             }
         });
+    }
+
+    private void init()
+    {
+        loginController = LoginController.getInstance(MainActivity2.this);
+        btnConsultation = (Button) findViewById(R.id.btnConsultation);
+        etPassword = (EditText) findViewById(R.id.nomutilisateur);
+        etUserName = (EditText) findViewById(R.id.mdp);
     }
 }
